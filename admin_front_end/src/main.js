5821c06e5e3ed4ba6ee86e36cd6a0f2c0a1f171e
Code002 <kakashivncm123@gmail.com>
2025-11-26 17:19:11 +0700

    Khoi tao du an, hieu chinh config ban dau

--- admin_front_end/src/main.js
+++ admin_front_end/src/main.js
@@ -1,6 +1,18 @@
-import './assets/main.css'
+import router from "./router";
+import setupVeeValidate from "./plugins/vee-validate";
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+// Toast
+import Toast from "@/utils/toast";
 
-createApp(App).mount('#app')
+// CSS
+import "./assets/styles/global.css";
+
+const app = createApp(App);
+
+// Đăng ký toast toàn cục
+app.provide("toast", Toast);
+
+setupVeeValidate();
+app.use(router).mount("#app");
