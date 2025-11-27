c3bc5f83e231908d2efd27c3ef0d9f3549bbd815
Code002 <kakashivncm123@gmail.com>
2025-11-28 04:53:07 +0700

    Cap nhat App cho backend

--- back_end/app.js
+++ back_end/app.js
@@ -1,19 +1,75 @@
 const express = require("express");
 const cors = require("cors");
-const contactsRouter = require("./app/routes/contact.route");
-const ApiError = require("./app/api-error");
+const fileUpload = require("express-fileupload");
+const tacgiaRouter = require("./app/routes/tacgia.route");
+const theloaiRouter = require("./app/routes/theloai.route");
+const nhaxuatbanRouter = require("./app/routes/nhaxuatban.route");
+const sachRouter = require("./app/routes/sach.route");
+const phieumuonRouter = require("./app/routes/phieumuon.route");
+const docgiaRouter = require("./app/routes/docgia.route");
+const authRouter = require("./app/routes/auth.route");
 
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const ApiError = require("./app/api-error");
+require("dotenv").config();
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.ORIGIN_ADMIN, process.env.ORIGIN_USER],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to contact book application." });
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 },
+  })
+);
+
+app.use((req, res, next) => {
+  req.session.users = req.session.users || {};
+  const referer = (req.headers.referer || "").toLowerCase();
+  if (referer.includes(process.env.PORT_ADMIN)) {
+    req.user = req.session.users.admin || null;
+  } else if (referer.includes(process.env.PORT_USER)) {
+    req.user = req.session.users.docgia || null;
+  } else {
+    req.user = req.session.users.admin || req.session.users.docgia || null;
+  }
+
+  console.log("Current user:", req.user?.HoTen, req.user?.type);
+  console.log("Data user:", req.user);
+
+  next();
 });
 
-// Router
-app.use("/api/contacts", contactsRouter);
+app.use(
+  fileUpload({
+    limits: { fileSize: 10 * 1024 * 1024 },
+    abortOnLimit: true,
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+// Đọc ảnh
+app.use("/uploads", express.static("public/uploads"));
+
+// Route
+app.use("/api/tacgia", tacgiaRouter);
+app.use("/api/nhaxuatban", nhaxuatbanRouter);
+app.use("/api/theloai", theloaiRouter);
+app.use("/api/sach", sachRouter);
+app.use("/api/phieumuon", phieumuonRouter);
+app.use("/api/docgia", docgiaRouter);
+app.use("/api/auth", authRouter);
 
 // handle 404 respone
 app.use((req, res, next) => {
